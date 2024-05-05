a1588659b88e205adadb8bc4dfd30963f43586d6
佐藤昭文 <akfm.sato@gmail.com>
2024-05-05 14:15:21 +0900

    test整理

--- fizz_buzz_test.ts
+++ fizz_buzz_test.ts
@@ -1,40 +1,95 @@
-import { assertEquals } from "jsr:@std/assert";
-import { test } from "jsr:@std/testing/bdd";
+import { assertEquals } from "jsr:@std/assert@0.224.0";
+import { test, describe } from "jsr:@std/testing@0.224.0/bdd";
 import { fizzBuzz } from "./fizz_buzz.ts";
 
-test("3の倍数の場合はFizzを返す", () => {
-  // Act
-  const result = fizzBuzz(3);
-  // Assert
-  assertEquals(result, "Fizz");
-});
+describe("3の倍数の場合はFizzを返す", () => {
+  test("引数: 3", () => {
+    // Act
+    const result = fizzBuzz(3);
+    // Assert
+    assertEquals(result, "Fizz");
+  })
 
-test("5の倍数の場合はBuzzを返す", () => {
-  // Act
-  const result = fizzBuzz(5);
-  // Assert
-  assertEquals(result, "Buzz");
-});
+  test("引数: 6", () => {
+    // Act
+    const result = fizzBuzz(6);
+    // Assert
+    assertEquals(result, "Fizz");
+  })
 
-test("3と5の倍数の場合はFizzBuzzを返す", () => {
-// Act
-  const result = fizzBuzz(15);
-  // Assert
-  assertEquals(result, "FizzBuzz");
-});
+  test("引数: 9", () => {
+    // Act
+    const result = fizzBuzz(9);
+    // Assert
+    assertEquals(result, "Fizz");
+  })
+})
 
-test("それ以外の場合はそのままの数値を返す", () => {
-  // Act
-  const result = fizzBuzz(1);
-  // Assert
-  assertEquals(result, "1");
-});
+describe("5の倍数の場合はBuzzを返す", () => {
+  test("引数: 5", () => {
+    // Act
+    const result = fizzBuzz(5);
+    // Assert
+    assertEquals(result, "Buzz");
+  })
 
-/**
- * TODO リスト
- *
- * - [x] 3の倍数の場合は"Fizz"を返す
- * - [x] 5の倍数の場合は"Buzz"を返す
- * - [x] 3と5の倍数の場合は"FizzBuzz"を返す
- * - [ ] それ以外の場合はそのままの数値を返す
- */
+  test("引数: 10", () => {
+    // Act
+    const result = fizzBuzz(10);
+    // Assert
+    assertEquals(result, "Buzz");
+  })
+
+  test("引数: 20", () => {
+    // Act
+    const result = fizzBuzz(20);
+    // Assert
+    assertEquals(result, "Buzz");
+  })
+})
+
+describe("3と5の倍数の場合はFizzBuzzを返す", () => {
+  test("引数: 15", () => {
+    // Act
+    const result = fizzBuzz(15);
+    // Assert
+    assertEquals(result, "FizzBuzz");
+  })
+
+  test("引数: 30", () => {
+    // Act
+    const result = fizzBuzz(30);
+    // Assert
+    assertEquals(result, "FizzBuzz");
+  })
+
+  test("引数: 45", () => {
+    // Act
+    const result = fizzBuzz(45);
+    // Assert
+    assertEquals(result, "FizzBuzz");
+  })
+})
+
+describe("それ以外の場合はそのままの数値を返す", () => {
+  test("引数: 1", () => {
+    // Act
+    const result = fizzBuzz(1);
+    // Assert
+    assertEquals(result, "1");
+  })
+
+  test("引数: 2", () => {
+    // Act
+    const result = fizzBuzz(2);
+    // Assert
+    assertEquals(result, "2");
+  })
+
+  test("引数: 4", () => {
+    // Act
+    const result = fizzBuzz(4);
+    // Assert
+    assertEquals(result, "4");
+  })
+})
